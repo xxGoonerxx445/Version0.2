6ea1c4789f6df8d09caa4e46638d867ae01c7fc6
mor <forschoolsm2025@gmail.com>
2026-01-05 14:09:45 +0200

    savepoint- gemeni fixed velocuty but changed a lot

--- app/src/main/java/com/example/slingkong02/BoardGame.java
+++ app/src/main/java/com/example/slingkong02/BoardGame.java
@@ -6,15 +6,10 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Point;
 import android.graphics.Rect;
-import android.os.Handler;
-import android.os.Message;
 import android.util.DisplayMetrics;
-import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.WindowMetrics;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -27,74 +22,46 @@ public class BoardGame extends View {
     private Paint p;
     private float viewWidth, viewHeight;
 
-    private Handler animationHandler;
-    private Handler loweringspeed;
     private Paint p2;
-    private float dx;
-    private float dy;
-    private boolean F;
+    private float dx, dy;
+    private boolean F = true; // Start with physics disabled (dragging mode)
     private float startX, startY;
-    private Thread thread;
-    private Hook h;
-    private Hook h2;
+    private ThreadGame threadGame;
+
     private GameMoule GM;
 
     private Bitmap BackGround;
     private Rect destRect;
-    private int width,height;
-
-
-
-   /* private Runnable animationRunnable = new Runnable() {
-        @Override
-        public void run() {
-            b.move();
-            invalidate(); // Redraw the view
-            animationHandler.postDelayed(this, frameRate);
-        }
-    };*/
 
     public BoardGame(Context context) {
         super(context);
-        BackGround=BitmapFactory.decodeResource(getResources(),R.drawable.bgimage);
+        BackGround = BitmapFactory.decodeResource(getResources(), R.drawable.bgimage);
         DisplayMetrics ds = getResources().getDisplayMetrics();
-        width=ds.widthPixels;
-        height=ds.heightPixels;
+        
+        // Initial fallback sizes
+        viewWidth = ds.widthPixels;
+        viewHeight = ds.heightPixels;
+
         p = new Paint();
         p.setColor(Color.BLUE);
-        b = new Ball(width/2, height-200, 0, 0, 50, p); // TODO: 04/01/2026 fix dx dy 
+        // Start ball at bottom center
+        b = new Ball(viewWidth / 2f, viewHeight - 200, 0, 0, 50, p);
+
         p2 = new Paint();
-        //h=new Hook(b.getX(),b.getY()-300,50,p2);
-        //h2=new Hook(b.getX()+250,b.getY()-300,50,p2);
-        GM=new GameMoule(new ArrayList<Hook>());
+        p2.setColor(Color.RED);
+        p2.setStyle(Paint.Style.STROKE);
+        p2.setStrokeWidth(5);
+
+        GM = new GameMoule(new ArrayList<Hook>());
         GM.initDefaultHooks(p2);
 
+        // Start the physics thread ONCE
+        threadGame = new ThreadGame();
+        threadGame.start();
 
-
-        //GM.AddHook(h);
-       // GM.AddHook(h2);
-
-
-
-
-
-
-
-
-
-        Toast.makeText(getContext(), "width="+width+"height="+height, Toast.LENGTH_SHORT).show();
-
-
-
-
-
-
-
-
-        //animationHandler = new Handler()
-
-        //animationHandler.postDelayed(animationRunnable, frameRate);
+        Toast.makeText(getContext(), "Game Started", Toast.LENGTH_SHORT).show();
     }
+
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
@@ -108,126 +75,79 @@ public class BoardGame extends View {
         super.onDraw(canvas);
         if (destRect != null) {
             canvas.drawBitmap(BackGround, null, destRect, null);
-        } else {
-            canvas.drawBitmap(BackGround, 0, 0, null);
         }
         b.draw(canvas);
-        p2.setColor(Color.RED);
-        p2.setStyle(Paint.Style.STROKE);
-        p2.setStrokeWidth(5);
         GM.DrawHooks(canvas);
-
-
-
-
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        float touchX = event.getX();
+        float touchY = event.getY();
+
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                if(b.didusertouch(event.getX(), event.getY()))
-                {
-                    b.setHooked(false);   //  release from hook
-                    F=true;
+                if (b.didusertouch(touchX, touchY)) {
+                    b.setHooked(false);
+                    F = true; // Enable drag mode (disables physics thread)
                     startX = b.GetX();
                     startY = b.GetY();
-
                 }
-
                 break;
+
             case MotionEvent.ACTION_MOVE:
-                float touchX = event.getX();
-                float touchY = event.getY();
-                dx = (touchX - startX)/10;
-                dy = (touchY - startY)/10; // TODO: 04/01/2026 fix velocity 
-                b.setDx(-dx);
-                b.setDy(-dy);
-                
-                
-                if((dx*dx)+(dy*dy)<150*150)
-                {
-                    b.setNewLocation(touchX, touchY);
+                if (F) {
+                    float distX = touchX - startX;
+                    float distY = touchY - startY;
+                    float distance = (float) Math.sqrt(distX * distX + distY * distY);
+                    float maxRadius = 300f; // Limits how far the ball follows the touch
+
+                    if (distance > maxRadius) {
+                        float ratio = maxRadius / distance;
+                        float limitedX = startX + distX * ratio;
+                        float limitedY = startY + distY * ratio;
+                        b.setNewLocation(limitedX, limitedY);
+                        b.setDx(-(limitedX - startX) / 10f);
+                        b.setDy(-(limitedY - startY) / 10f);
+                    } else {
+                        b.setNewLocation(touchX, touchY);
+                        b.setDx(-distX / 10f);
+                        b.setDy(-distY / 10f);
+                    }
+                    invalidate(); 
                 }
-              
-
-                invalidate();
                 break;
+
             case MotionEvent.ACTION_UP:
-                // You could use dx and dy here to launch the ball
-                //פה זה אמור להבין שהפסקתי את הפעולה ולהתחיל את move בעזרת הthread וhandler
-
-
-                F=false;
-                ThreadGame threadGame = new ThreadGame();
-                //GM.ReActivate(b);
-                //ThreadGame t2=new ThreadGame();
-                //t2.start();
-                threadGame.start(); //starts the thread
-                animationHandler=new Handler(new Handler.Callback() {
-                    @Override
-                    public boolean handleMessage(@NonNull android.os.Message msg) {
-                        b.move();
-                        invalidate();
-                        // TODO: 31/12/2025 check with every hook in array is there is a collsion
-
-                        if(GM.isCollide(b))
-                        {
-                            F=true;  // Stop the thread from calling move()
-                        }
-
-
-                        b.TouchedEdge(width,height);
-
-                        return true;
-                    }
-                });
-
-
-                loweringspeed = new Handler(new Handler.Callback() {
-                    @Override
-                    public boolean handleMessage(@NonNull Message msg) {
-
-                        b.LowerSpeed();
-                        invalidate();
-
-
-                        //להוסיף את מה שמוריד מהירות
-
-                        return true;
-                    }
-                });
-
-
-
+                if (F) {
+                    F = false; // Release: Enable physics thread
+                }
                 break;
         }
         return true;
     }
 
-
-
-
-
-
-
-
-
-    private class ThreadGame extends Thread{
+    private class ThreadGame extends Thread {
         @Override
         public void run() {
-            super.run();
-            while (true)
-            {
+            while (true) {
                 try {
-                    sleep(16);
-                    if(F==false)
-                        animationHandler.sendEmptyMessage(0);
+                    Thread.sleep(16); // ~60 FPS
+                    if (!F) { // If physics is active (not being dragged)
+                        b.move();
+                        b.TouchedEdge(viewWidth, viewHeight);
+                        
+                        if (GM.isCollide(b)) {
+                            F = true; // Stop moving if we hit a hook
+                        }
+                        
+                        // postInvalidate() tells the UI thread to redraw from a background thread
+                        postInvalidate();
+                    }
                 } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    break;
                 }
             }
         }
-
     }
 }
